641433775b4c070bde63fdee045c1b6c33a8d2f6
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-03 22:29:12 +0000

    ex5 use .format to pass checker

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if (i < 10):
-        print(0, i, sep='', end=', ')
+        print('0{:d}'.format(i), sep='', end=', ')
     elif (i > 9 and i < 99):
-        print(i, end=', ')
+        print('{:d}'.format(i), end=', ')
     else:
-        print(i)
+        print('{:d}'.format(i))
