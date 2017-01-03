9cb149649ac56ce7baa4c83b8c445eeae4fc70ba
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-03 22:33:43 +0000

    ex5 only two print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if (i < 10):
-        print('0{:d}'.format(i), sep='', end=', ')
-    elif (i > 9 and i < 99):
-        print('{:d}'.format(i), end=', ')
-    else:
-        print('{:d}'.format(i))
+for i in range(0, 99):
+    print('{:02d}'.format(i), end=', ')
+print('{:d}'.format(99))
