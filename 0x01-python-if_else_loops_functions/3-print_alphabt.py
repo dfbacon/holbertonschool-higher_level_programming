8952c09939176c5000ed2fbb8ee4bdc31d869ae6
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-03 22:17:20 +0000

    ex3 string instead of range

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 i = 0
-for i in range(ord('a'), ord('z')+1):
-    if (not(i == ord('e') or i == ord('q'))):
-        print(chr(i), end='')
+for i in 'abcdfghijklmnoprstuvwxyz':
+        print('{:s}'.format(i), end='')
