2a57db2363605d77fddac8164cfdd87a8017b89c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-03 23:09:08 +0000

    dumb mistake, missing a 6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ elif last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than"
           " 5".format(number, last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is less than and not"
+    print("Last digit of {:d} is {:d} and is less than 6 and not"
           " 0".format(number, last_digit))
