f11324df761271867345cc5e7c4c9912c77a24e9
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-10 21:15:52 +0000

    ex1 none check?

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx > len(my_list) - 1 or idx <= -len(my_list) - 1:
         return (None)
-    if my_list == []:
+    if my_list == None:
         return (None)
     return (my_list[idx])
