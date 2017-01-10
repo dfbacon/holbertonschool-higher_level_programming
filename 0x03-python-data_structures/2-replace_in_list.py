580d93f6f7826da821d7558dfe31b5b44862fcb3
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-10 21:13:14 +0000

    ex2 negative index check

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (not(idx > len(my_list) - 1)):
-        my_list[idx] = element
+    if idx > len(my_list) - 1 or idx <= len(my_list) -1:
+        return (my_list)
+    my_list[idx] = element
     return (my_list)
