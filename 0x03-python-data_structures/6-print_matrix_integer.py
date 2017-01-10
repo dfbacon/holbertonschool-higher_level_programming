7608a5cac24672b20c7f7039fe089dcf363868e2
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-10 23:33:40 +0000

    ex6 working

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    i = 0
     for row in matrix:
-        for item in matrix[i]:
-            print('{:d}'.format(item), sep=' ', end='\n')
+        for item in row:
+            print('{:d}'.format(item), end='')
+            if row.index(item) < len(row) - 1:
+                print(' ', end='')
         print('')
-        i += 1
