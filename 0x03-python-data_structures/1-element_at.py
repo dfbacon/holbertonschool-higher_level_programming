57a51ce61b473b315555f0492d100c36746ad4f3
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-10 21:18:25 +0000

    ex1 idx < 0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list) - 1 or idx <= -len(my_list) - 1:
-        return (None)
-    if my_list == [None]:
+    if idx > len(my_list) - 1 or idx <= -len(my_list) - 1 or idx < 0:
         return (None)
     return (my_list[idx])
