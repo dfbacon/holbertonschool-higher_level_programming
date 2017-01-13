1897f4f2a3d9cf8c3d8e8316bc335afd90aaca19
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-13 07:56:49 +0000

    ex100 empty dict and None check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None or my_list is []:
+        return(0)
     avg = sum(i[0] * i[1] for i in my_list)
     avg /= sum(i[1] for i in my_list)
     return(avg)
