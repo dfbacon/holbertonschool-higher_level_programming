74c9abb40c598b6faa20a3a69620aa0b9f09fbc9
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-13 06:14:33 +0000

    ex6 damn checker

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     if my_dict is None:
-        return(None)
+        return (None)
     for key, val in sorted(my_dict.items()):
-        print('{} : {}'.format(key, val))
+        print("{}: {}".format(key, val))
