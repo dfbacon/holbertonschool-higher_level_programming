7c5117db1d2097f4886620355f8f8a9c5f98923e
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-13 07:58:38 +0000

    ex100 empty dict check edit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is []:
+    if my_list == []:
         return(0)
     avg = sum(i[0] * i[1] for i in my_list)
     avg /= sum(i[1] for i in my_list)
