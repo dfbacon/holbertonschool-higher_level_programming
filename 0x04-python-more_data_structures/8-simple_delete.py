b13a2b3068bdcca2a295c6e44ccf6560d48b9e24
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-13 06:38:59 +0000

    ex8 remove excess colon

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(my_dict, key=""):
     if my_dict is None:
-        return(None):
+        return(None)
     if key in my_dict:
         del my_dict[key]
     return(my_dict)
