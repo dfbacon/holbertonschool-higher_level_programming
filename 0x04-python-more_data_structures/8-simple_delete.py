cde88188324b75d6dc5564c14a830ff3415ae985
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-13 06:37:45 +0000

    ex8 cleaned up

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,9 +2,6 @@
 def simple_delete(my_dict, key=""):
     if my_dict is None:
         return(None):
-    if key is None or key is "":
-        return(None)
-    elif key not in my_dict:
-        return(None)
-    del my_dict[key]
+    if key in my_dict:
+        del my_dict[key]
     return(my_dict)
