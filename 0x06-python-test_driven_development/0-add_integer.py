bbceb9b42132356f936d6f87d7461ad82f3d3604
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-17 23:01:02 +0000

    ex0 with internal documentation

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+'''
+This is the "add_integer" module.
+
+The add_integer module supplies one function, add_integer(a, b).
+
+For example:
+>>> add_integer(1, 3)
+4
+'''
+
 def add_integer(a, b):
+    '''
+    Return the sum of a and b, an exact integer.
+    '''
+
     if isinstance( a, float ) or isinstance( b, float ) is True:
         a, b = int(a), int(b)
     if isinstance( a, int ) is False:
