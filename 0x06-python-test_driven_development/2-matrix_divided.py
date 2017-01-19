1e7e3e86087bfd15dcf1e36a933a724c04efdc24
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-19 01:46:24 +0000

    ex2 working with round, added exceptions

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 '''
-This is the "matrix_divided module.
+This is the "matrix_divided" module.
 
 The matrix_divided module supplies one function, matrix_divided(matrix, div).
 
 '''
+
+
 def matrix_divided(matrix, div):
     '''Return matrix with each value divided by @div
+
+    matrix: matrix being evaluated
+    div: divisor
     '''
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix)):
-            matrix[j][i] /= div
-            format(matrix[j][i], '.2f')
-    return(matrix)
+
+    if matrix is []:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        if i is []:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(i) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in i:
+            if isinstance(j, (int, float)) is False:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if isinstance(div, (int, float)) is False:
+        raise TypeError("div must be a number")
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
+    new = []
+    temp = []
+    for i in matrix:
+        for j in i:
+            temp.append(round(j / div, 2))
+        new.append(temp)
+    return(new)
 
 matrix = [
     [1, 2, 3],
