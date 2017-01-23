af10976726602665540f4a0924ec30e5bd2ab13a
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-23 22:07:45 +0000

    ex2 pythonic but not working correctly

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        self.__set_size(size)
+        self.__size = size
 
-    def __get_size(self):
+    @property
+    def size(self):
         return self.__size
 
-    def __set_size(self, size):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+    @size.setter
+    def size(self, size):
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
