535d8a756cde43b2625474e843c54c3295e8dee5
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-23 21:23:18 +0000

    ex1 less pythonic

--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
-        Square.__size = size
+        self.__set_size(size)
+
+    def __get_size(self):
+        return self.__size
+
+    def __set_size(self, size):
+        self.__size = size
