c7fb10111ea6b50b57dd310936c2e8526f55ed23
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-24 20:08:33 +0000

    ex4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width is 0 or self.height is 0:
+            return 0
+        return((self.width * 2) + (self.height *2))
+
+    def __str__(self):
+        temp = ""
+        if self.height is 0 or self.width is 0:
+            return temp
+        for i in range(self.height):
+            for j in range(self.width):
+                temp += '#'
+            temp += '\n'
+        temp = temp[:-1]
+        return temp
+
+    def __repr__(self):
+        width = str(self.__width)
+        height = str(self.__height)
+        return 'Rectangle(' + width + ', ' + height + ')'
