d6d165094916cb3a8160f6df31aaa728df846dbc
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-24 07:12:47 +0000

    ex4

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -2,9 +2,12 @@
 '''
 This is the "4-square" module.
 
-The 4-square module supplies one class and **number** of methods.
+The 4-square module supplies one class and four  methods.
 Class: Square
-Methods:
+Methods: __init__(self, size=0)
+         size(self)
+         size(self, value)
+         area(self)
 
 '''
 
@@ -18,14 +21,20 @@ class Square:
     '''
 
     def __init__(self, size=0):
+        '''The __init__ method initializes the size object
+        '''
         self.size = size
 
     @property
     def size(self):
+        '''The size method sets size object as private
+        '''
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''The size method checks value for type int and value >= 0
+        '''
         if not(isinstance(value, int)):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -34,4 +43,6 @@ class Square:
             self.__size = value
 
     def area(self):
+        '''The area function returns the area (size**2) of the square
+        '''
         return self.size ** 2
