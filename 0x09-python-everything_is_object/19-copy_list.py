9f131da3c614507561f5d0386a3264f86e142c22
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-28 23:22:37 +0000

    ex19 remove documentation

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-'''
-This is the "19-copy_list" module.
-
-The copy_list module contains one function "copy_list(l)".
-
-'''
-
-
 def copy_list(l):
-    '''This is the "copy_list" function.
-
-    @l: The list to be copied.
-    The function returns a copy of a list containing any type of object.
-    '''
-
-    return l[:]
+    return (l[:])
