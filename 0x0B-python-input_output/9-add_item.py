4d23a53e8bb6268a7fee9decace53ce1c87bfd3d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-03 00:11:29 +0000

    ex9 working

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 import json
 import sys
-import os.path
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 argv = sys.argv
-my_file = 'add_item.json'
 
 try:
-    fn = open(my_file)
-    fn.close()
+    fn = load_from_json_file('add_item.json')
 except:
-    fn = open(my_file, 'w+')
+    fn = open('add_item.json', 'w+')
+    empty = []
+    save_to_json_file(empty, 'add_item.json')
     fn.close()
 
-with open(my_file, "r+") as fn:
-    if os.path.isfile(my_file):
-        temp = load_from_json_file(my_file)
-    else:
-        temp = []
-    if len(argv) > 0:
-        for i in argv:
-            temp.append(argv[i])
-    save_to_json_file(temp, my_file)
+temp = load_from_json_file('add_item.json')
+if len(argv) > 0:
+    for i in range(1, len(argv)):
+        temp.append(argv[i])
+    save_to_json_file(temp, 'add_item.json')
