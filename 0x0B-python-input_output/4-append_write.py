72c5135edf8b5342d2e5d1e29467b9745e8e3ad0
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-06 18:26:37 +0000

    ex4 add mode declaratione

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    with open(filename, 'w', encoding='utf-8') as fn:
+    with open(filename, mode='w', encoding='UTF-8') as fn:
         return fn.write(text)
