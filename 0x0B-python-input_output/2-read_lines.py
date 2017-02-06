f661676cb62d955f2815b28071dab9039dd89413
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-06 18:21:33 +0000

    ex2 school formatting

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,7 +2,7 @@
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding='utf-8') as fn:
         if nb_lines <= 0:
-            print (fn.read(), end='')
+            print(fn.read(), end='')
         else:
             for i in range(nb_lines):
-                print (fn.readline(), end='')
+                print(fn.readline(), end='')
