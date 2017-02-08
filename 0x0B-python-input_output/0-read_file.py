7ce45cc84c67eca158ce2f71a9bde0bf140bde18
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-08 19:23:17 +0000

    ex0 use read(); change print formatting

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as fn:
-        for line in fn:
-            print (line, end=''))
+    with open(filename, 'r', encoding='utf-8') as fn:
+        fn_data = f.read()
+    print ("{}".format(fn_data), end='')
