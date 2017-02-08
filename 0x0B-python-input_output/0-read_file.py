202bb771a0092595bfd24c44d97d1aeac6fb200a
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-08 19:24:02 +0000

    ex0 pep8 compliant

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as fn:
         fn_data = f.read()
-    print ("{}".format(fn_data), end='')
+    print("{}".format(fn_data), end='')
