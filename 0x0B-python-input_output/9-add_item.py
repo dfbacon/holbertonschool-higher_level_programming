99e95e88699925829cc1ab0e18104605357dc88d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-08 19:17:57 +0000

    ex9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import json
 import sys
-
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 argv = sys.argv
+
 
 try:
     fn = load_from_json_file('add_item.json')
