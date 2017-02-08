bdaeb92e239dd6da44957f678c0af8aa7e15d435
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-08 19:20:03 +0000

    ex0 add for loop; change print statement

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, encoding='utf-8') as fn:
-        print (fn.read())
+        for line in fn:
+            print (line, end=''))
