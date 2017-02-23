b44e16eb712ab913c799bd9670433c10e3d21fcd
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-23 19:28:50 +0000

    change count

--- 0x0C-SQL_introduction/15-groups.sql
+++ 0x0C-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- List the number of records with same score in 'second_table'.
 
-SELECT score, COUNT(*) AS "number"
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
-ORDER BY "number" DESC;
+ORDER BY number DESC;
