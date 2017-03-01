2745f0f2df4396b26a32b4bb4c574b70ee1ef436
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-01 07:36:36 +0000

    add missing s

--- 0x0D-SQL_more_queries/15-comedy_only.sql
+++ 0x0D-SQL_more_queries/15-comedy_only.sql
@@ -2,5 +2,5 @@
 
 SELECT tv_shows.title FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_show.id
-WHERE tv_genres.name = 'Comedy' ORDER BY tv_show.title ASC;
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
