99d50b156a14976faa9e1a8438dcd6288a6756d5
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-08 20:59:38 +0000

    ex2

---
+++ 0x0E-select_states.py/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This is the '2-my_filter_states' module.
+
+2-my_filter_states uses MySQLdb to access a database (passed as argv[3])
+filters for table.name to match argv[4] and prints the results.
+
+'''
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost",
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3])
+cur = db.cursor()
+query = "SELECT * FROM states\
+ WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4])
+cur.execute(query)
+for row in cur.fetchall():
+    print(row)
+cur.close()
+db.close()
