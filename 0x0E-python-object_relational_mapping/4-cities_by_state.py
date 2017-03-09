46cdec68255d09d9e259bbcbfe14e6e3ece2ea8a
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-09 07:32:01 +0000

    add name == main

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -6,17 +6,18 @@ This is the '4-cities_by_state' module.
 lists all cities and prints the results.
 
 '''
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host="localhost",
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
- INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
-for row in cur.fetchall():
-    print(row)
-cur.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
