8aa13030d86276525131bba2d35c20b7e687ed81
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-09 07:33:43 +0000

    add name == main

--- 0x0E-python-object_relational_mapping/5-filter_cities.py
+++ 0x0E-python-object_relational_mapping/5-filter_cities.py
@@ -6,23 +6,24 @@ This is the '5-filter_cities' module.
 filters cities matched to given state (argv[4]) and prints the results.
 
 '''
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host="localhost",
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cur = db.cursor()
-test = cur.execute("SELECT cities.name, states.name FROM cities\
- INNER JOIN states ON states.id = cities.state_id\
- WHERE states.name LIKE %s\
- ORDER BY cities.id ASC", (sys.argv[4],))
-if test == 0:
-    print("No injection without protection.")
-cities = []
-for row in cur.fetchall():
-    cities.append(row[0])
-print(', '.join(cities))
-cur.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    test = cur.execute("SELECT cities.name, states.name FROM cities\
+    INNER JOIN states ON states.id = cities.state_id\
+    WHERE states.name LIKE %s\
+    ORDER BY cities.id ASC", (sys.argv[4],))
+    if test == 0:
+        print("No injection without protection.")
+    cities = []
+    for row in cur.fetchall():
+        cities.append(row[0])
+    print(', '.join(cities))
+    cur.close()
+    db.close()
