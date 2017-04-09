f7673368911a7850165e4b9ba30df6c9758cb51b
Daniel Bacon <dbacon338@gmail.com>
2017-04-09 03:41:17 +0000

    total redo

--- 0x0F-python-network_0/39-methods.sh
+++ 0x0F-python-network_0/39-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl -SI $1 | grep "Accept:" | cut -d ':' -f2
+curl -siIX OPTIONS "$1" | grep Allow: | cut -d' ' -f2-
