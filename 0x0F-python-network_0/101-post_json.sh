13604f0f2e4740996a22b67f7e437cc9036cccc3
Daniel Bacon <dbacon338@gmail.com>
2017-04-09 04:01:52 +0000

    edit flags

--- 0x0F-python-network_0/101-post_json.sh
+++ 0x0F-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -HsLXT "Content-Type: application/json" POST "$2" "$1"
+curl -H "Content-Type: application/json" -sLXT POST "$2" "$1"
