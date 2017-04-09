72df041269b1c70a26daee580d5d52a58e19c455
Daniel Bacon <dbacon338@gmail.com>
2017-04-09 09:10:35 +0000

    url request

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -16,3 +16,9 @@ $ ./4-hbtn_status.py
 '''
 
 if __name__ == "__main__":
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
