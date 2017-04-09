42e24f5b16714be94989ea808b265d527d5cb656
Daniel Bacon <dbacon338@gmail.com>
2017-04-09 09:02:51 +0000

    display url body

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -20,3 +20,18 @@ $ ./3-error_code.py domain_name
 '''
 
 if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: ./3-error_code.py domain_name")
+        exit(1)
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            page = (response.read()).decode('utf-8')
+            print(page)
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code)
