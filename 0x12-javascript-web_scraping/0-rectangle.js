13f4fe8b5ee1ad3fdfc1f2ca4a6f930bcb50bcf9
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 05:30:39 +0000

    create empty Rectangle class

---
+++ 0x12-javascript-web_scraping/0-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+Write an empty class Rectangle that defines a rectangle.
+* 0-main.js:
+    #!/usr/bin/node
+    const Rectangle = require('./0-rectangle').Rectangle;
+
+    const r1 = new Rectangle();
+    console.log(r1);
+    console.log(r1.constructor);
+
+Requirements:
+* Must use the `function` notation for defining a class.
+
+*/
+
+module.exports = {
+  Rectangle: function () {}
+};
