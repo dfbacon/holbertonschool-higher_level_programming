a7e4746e885b02989db50149c938f476777ef3ab
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 10:52:32 +0000

    sort api data

---
+++ 0x12-javascript-web_scraping/13-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/*
+
+Write a script that computes the number of tasks completed by user id.
+
+Requirements:
+* The first argument is the API URL.
+    https://jsonplaceholder.typicode.com/todos
+
+*/
+
+const request = require('request');
+var url = process.argv[2] || '';
+var completed = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+
+  let data = JSON.parse(body);
+  for (let i = 0; i < data.length; i++) {
+    let id = data[i].userId;
+    if (data[i].completed) {
+      completed[id] = (completed[id] || 0) + 1;
+    }
+  }
+
+  console.log(completed);
+});
