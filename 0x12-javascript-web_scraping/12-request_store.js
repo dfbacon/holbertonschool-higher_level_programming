539e6bdf487f97e22e236da3500731ca4d4a1209
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 10:37:24 +0000

    write url body to file

---
+++ 0x12-javascript-web_scraping/12-request_store.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*
+
+Write a script that gets the contents of a webpage and stores it in a file.
+
+Requirements:
+* The first argument is the URL to request.
+* The second argument is the file path to store the body response.
+* The file must be UTF-8 encoded.
+
+*/
+
+const fs = require('fs');
+const request = require('request');
+var url = process.argv[2] || '';
+var file = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+
+  fs.writeFile(file, body, 'utf8', function (error) {
+    if (error) {
+      return console.log(error);
+    }
+  });
+});
