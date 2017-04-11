5293dd6756526d49e1e77ef111c5481c9878c71d
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 05:38:37 +0000

    set Rectangle class with height and width

---
+++ 0x12-javascript-web_scraping/1-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle.
+* 1-main.js:
+    #!/usr/bin/node
+    const Rectangle = require('./1-rectangle').Rectangle;
+
+    const r1 = new Rectangle(2, 3);
+    console.log(r1);
+    console.log(r1.width);
+    console.log(r1.height);
+
+    const r2 = new Rectangle(2, -3);
+    console.log(r2);
+    console.log(r2.width);
+    console.log(r2.height);
+
+    const r3 = new Rectangle(2);
+    console.log(r3);
+    console.log(r3.width);
+    console.log(r3.height);
+
+Requirements:
+* Must use the `function` notation for defining a class.
+* Constructor must take 2 arguments: w and h.
+* Initialize the instance attribute `width` with the value of w.
+* Initialize the instance attribute `height` with the value of h.
+
+*/
+
+module.exports = {
+  Rectangle: function (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
