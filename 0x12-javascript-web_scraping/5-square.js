6184b2ae466e593428bec7638363cead0fe1ea92
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 06:10:39 +0000

    create square class; inherits from rectangle

---
+++ 0x12-javascript-web_scraping/5-square.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+/*
+Write a class `Square` that defines a square and inherits from `Rectangle` of 4-rectangle.js
+* 5-main.js:
+    #!/usr/bin/node
+    const Square = require('./5-square').Square;
+
+    const s1 = new Square(4);
+    s1.print();
+    s1.double();
+    s1.print();
+
+Requirements:
+* Must use the `function` notation for defining a class.
+* Constructor must take 1 argument: size.
+* Constructor `Rectangle` must be called.
+
+*/
+
+function Rectangle (w, h) {
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+
+  this.print = function () {
+    for (let x = 0; x < this.height; x++) {
+      console.log(('X').repeat(this.width));
+    }
+  };
+
+  this.rotate = function () {
+    [this.width, this.height] = [this.height, this.width];
+  };
+
+  this.double = function () {
+    this.width *= 2;
+    this.height *= 2;
+  };
+}
+
+module.exports = {
+  Square: function (size) {
+    Rectangle.call(this, size, size);
+  }
+};
