e9fa6ecfb6fc0d1fdffd036ec576fea325c083fe
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 10:04:15 +0000

    star wars api

---
+++ 0x12-javascript-web_scraping/10-starwars_title.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/*
+
+Write a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+
+Requirements:
+* The first argument is the episode number.
+* Must use the Star Wars API (swapi.co) with the endpoint:
+    http://swapi.co/api/films/:episode_id
+
+*/
+
+const request = require('request');
+var id = parseInt(process.argv[2]);
+var url = 'http://swapi.co/api/films/' + id;
+
+if (id < 8) {
+  request(url, function (error, response, body) {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(JSON.parse(body).title);
+  });
+}
