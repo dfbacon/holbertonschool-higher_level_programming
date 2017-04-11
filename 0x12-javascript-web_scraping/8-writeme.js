7c3aca125d10b146e44374b511b7c4085b3783e1
Daniel Bacon <dbacon338@gmail.com>
2017-04-11 09:23:19 +0000

    write to file

---
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+
+Write a script that writes a string to a file.
+
+Requirements:
+* The first argument is the file path.
+* The second argument is the string to write.
+* The content of the file must be read in `utf-8`.
+* If an error occurres during writing, print the error object.
+
+*/
+
+const fs = require('fs');
+var file = process.argv[2] || '';
+var string = process.argv[3] || '';
+
+fs.writeFile(file, string, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+});
