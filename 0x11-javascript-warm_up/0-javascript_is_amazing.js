0de3be5a08edf97f940a8f74af090aad80e7e315
Daniel Bacon <dbacon338@gmail.com>
2017-04-27 19:58:59 +0000

    add comments

--- 0x11-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x11-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
+/*
+Write a script that prints "Javascript is amazing".
+
+Requirements:
+* You must create a constant variable called myVar with the value,
+"Javascript is amazing"
+
+* You must use console.log(...) to print all output.
+* Not allowed to use var.
+*/
+
 const myVar = 'Javascript is amazing';
 console.log(myVar);
