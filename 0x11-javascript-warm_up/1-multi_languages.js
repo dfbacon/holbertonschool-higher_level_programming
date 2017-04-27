7f22eee0a81397430337d83978f8e5436c24e53e
Daniel Bacon <dbacon338@gmail.com>
2017-04-27 20:01:04 +0000

    print three lines

---
+++ 0x11-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Write a script that prints 3 lines.
+
+Requirements:
+* Must use console.log(...) to print all output.
+* Not allowed to use var
+
+*/
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
