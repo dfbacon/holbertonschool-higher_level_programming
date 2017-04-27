5a4f77bb706f03556fa7623766ed7f58cc6786f5
Daniel Bacon <dbacon338@gmail.com>
2017-04-27 20:32:04 +0000

    comments

---
+++ 0x11-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Write a script that prints 3 lines: (like 1-multi_languages.js) but by using
+an array of string and a loop.
+
+Requirements:
+* You must use console.log(...) to print all output
+* You are not allowed to use var
+* You are not allowed to use any if/else statements
+* You are only allowed to use console.log one time
+* You must use a loop
+
+*/
