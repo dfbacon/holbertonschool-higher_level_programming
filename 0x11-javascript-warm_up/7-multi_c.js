878b2dd33fa0899ad44a18b5e9cff157cda2b7e6
Daniel Bacon <dbacon338@gmail.com>
2017-04-27 22:09:41 +0000

    print multiple times

---
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+Write a script that prints x times "C is fun".
+
+Requirements:
+* Where x is the first argument of the script
+* If the first argument can't be converted to an
+integer, print "Missing number of occurrences"
+
+* You must use console.log(...) to print all output
+* You are not allowed to use var
+* You are only allowed to use console.log one time
+* You must use a loop
+
+*/
+
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < s; i++) {
+    console.log('C is fun');
+  }
+}
