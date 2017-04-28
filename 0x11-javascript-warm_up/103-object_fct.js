68eb2d234fb5f91ba10adf280e992f2591c32b9d
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:24:10 +0000

    implement increment function

---
+++ 0x11-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+/*
+Update the 103-object_fct.js script by adding a new function `incr`
+that increments the integer value.
+
+Base code:
+  #!/usr/bin/node
+  let myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+
+  // YOUR CODE HERE
+
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+
+
+Requirements:
+* You are not allowed to use var
+
+*/
+
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  return this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
