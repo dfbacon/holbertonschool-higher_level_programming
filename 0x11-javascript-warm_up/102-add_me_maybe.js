cfc63ea0101cece89edd73c9c21cff7ffaa8961b
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:19:19 +0000

    add comments

--- 0x11-javascript-warm_up/102-add_me_maybe.js
+++ 0x11-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,22 @@
 #!/usr/bin/node
+/*
+Write a function that increments and calls a function.
+
+* 102-main.js
+    #!/usr/bin/node
+    const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+    addMeMaybe(4, function (nb) {
+      console.log('New value: ' + nb);
+    });
+
+Requirements:
+* The function must be visible from outside.
+* Prototype:
+    function(number, theFunction)
+* You are not allowed to use var
+
+*/
+
 module.exports = {
   addMeMaybe: function (n, f) {
     return f(n + 1);
