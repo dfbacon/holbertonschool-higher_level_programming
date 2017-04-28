977c5db961884a2a4f504d0c3e4756f493f8dc40
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:16:53 +0000

    add comments

--- 0x11-javascript-warm_up/101-call_me_moby.js
+++ 0x11-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,23 @@
 #!/usr/bin/node
+/*
+Write a function that executes x times a function.
+
+* 101-main.js
+    #!/usr/bin/node
+    const callMeMoby = require('./101-call_me_moby').callMeMoby;
+    callMeMoby(3, function () {
+      console.log('C is fun');
+    });
+
+Requirements:
+* The function must be visible from outside.
+* Prototype:
+    function (x, theFunction)
+
+* You are not allowed to use var
+
+*/
+
 module.exports = {
   callMeMoby: function (i, f) {
     for (let x = 0; x < i; x++) {
