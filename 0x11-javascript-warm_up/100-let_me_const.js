aca0e4421c754fbb07a3f71765e159b799e2beb1
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:14:41 +0000

    add comments

--- 0x11-javascript-warm_up/100-let_me_const.js
+++ 0x11-javascript-warm_up/100-let_me_const.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
+/*
+Write a file that modifies the value of myVar to 333.
+
+* 100-main.js
+    #!/usr/bin/node
+    myVar = 89;
+    require('./100-let_me_const')
+    console.log(myVar);
+*/
+
 myVar = 333;
